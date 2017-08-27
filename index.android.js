f152411711bd233b82a5e01518f884f4556c0fd8
Herb Hagely <cpt.tenneal@gmail.com>
2017-08-27 18:37:55 -0500

    Checking in progress
    just checking in some basic progress on the course.

--- index.android.js
+++ index.android.js
@@ -1,12 +1,12 @@
 // Import a library to help create a component
 import React from 'react';
-import ReactNative from 'react-native';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
 
 // Create a component
-const App = () => {
-  return (
-    <Text>Some Text</Text>
-  );
-}
+const App = () => (
+  <Header headerText={ 'Albums' } />
+);
 
 // Render it to the device
+AppRegistry.registerComponent('albums', () => App);
