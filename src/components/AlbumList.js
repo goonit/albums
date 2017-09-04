31fd8c757b953788abf9b7a28bdb44f758baef3b
Herb <cpt.tenneal@gmail.com>
2017-09-03 22:36:18 -0500

    Completed albums app

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView, Text } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
+    state = { albums: [] };
+
     componentWillMount() {
-        // fetch('https://rallycoding.herokuapp.com/api/music_albums')
-        // .then(response => response.json())
-        // .then(responseData => console.log(responseData));
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-            .then(response => console.log(response));
+            .then(response => this.setState({ albums: response.data }));
+    }
+
+    renderAlbums() {
+        return (this.state.albums.map((album) => 
+            <AlbumDetail key={album.title} album={album} />));
     }
 
     render() {
+        console.log(this.state);
+
         return (
-            <View>
-                <Text>AlbumList</Text>
-            </View>
+            <ScrollView>
+                {this.renderAlbums()}
+            </ScrollView>
         );
     }
-
-    
 }
 
 export default AlbumList;
